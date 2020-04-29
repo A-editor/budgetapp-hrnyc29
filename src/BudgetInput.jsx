4cc0db6a513e8553556522fa15662c91a2a3e921
Ahsan Awan <aawan235@gmail.com>
2020-04-28 20:03:38 -0400

    further front end development

---
+++ src/BudgetInput.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+class BudgetInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputcategory: "",
+      inputbudget: "",
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    let name = event.target.name;
+    this.setState({ [name]: event.target.value });
+  }
+
+  handleClick() {
+    this.props.createCategory(this.state);
+  }
+
+  render() {
+    return (
+      <div className="category-form">
+        <div className="category-input">
+          <input
+            type="text"
+            name="inputcategory"
+            value={this.state.inputcategory}
+            onChange={this.handleChange}
+            placeholder="Budget Category"
+          />
+          <input
+            type="number"
+            name="inputbudget"
+            value={this.state.inputbudget}
+            onChange={this.handleChange}
+            placeholder="Target Budget"
+          />
+        </div>
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            this.handleClick();
+          }}
+        >
+          +
+        </button>
+      </div>
+    );
+  }
+}
+
+export default BudgetInput;
